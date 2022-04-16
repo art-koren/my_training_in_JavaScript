7e4e1d8692797eec546024cdeb904fa0b37dada3
Artem Kornev <art-kornev-ru@ya.ru>
2022-04-16 11:22:46 +0300

    applied the conditions

--- js/script.js
+++ js/script.js
@@ -5,4 +5,41 @@ let nam3 = 25;
 
 let result = nam1 + nam2 + nam3
 
-console.log('Результат:' + result);
+console.log('Результат:' + ' ' + result);
+
+
+
+
+/*-------------------------------------------*/
+
+
+
+
+let namber = 5
+
+if (namber == 5) {
+    console.log('Вау!');
+}
+
+
+
+namber = 6
+
+if (namber > 5) {
+    console.log('Ахуеть!');
+}
+
+
+
+namber = 4
+
+if (namber < 5 || namber > 0) {
+    console.log('Неплохо');
+}
+
+
+namber = -1
+
+if (namber == 0 || namber < 0 ) {
+    console.log('Очень плохо');
+}
